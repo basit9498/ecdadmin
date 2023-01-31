b92c8413fa3acc5ddc013c0716e874a0cf0a3265
Abdul Basit <basit9498@gmail.com>
2023-01-31 17:47:07 +0500

    Update index.js

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -139,21 +139,20 @@ const Index = () => {
 
             <Formik
               initialValues={{
-                email: "anddeveloper.abdulbasit@gmail.com",
-                password: "12345678",
+              
               }}
-              // validate={(values) => {
-              //   const errors = {};
-              //   if (!values.email) {
-              //     errors.email = 'Required';
-              //   } else if (
-              //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-              //   ) {
-              //     errors.email = 'Invalid email address';
-              //   }
+              validate={(values) => {
+                const errors = {};
+                if (!values.email) {
+                  errors.email = 'Required';
+                } else if (
+                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+                ) {
+                  errors.email = 'Invalid email address';
+                }
 
-              //   return errors;
-              // }}
+                return errors;
+              }}
               onSubmit={async (values) => {
                 dispatch(
                   loginUser({
